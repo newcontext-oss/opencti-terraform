6122caa71d28dfd16afa8b4472770869f67b038a
Colby Goettel <colby.goettel@newcontext.com>
2021-02-09 11:20:10 -0600

    pre-deploy mk2

--- azure/vm.tf
+++ azure/vm.tf
@@ -6,6 +6,11 @@ resource "tls_private_key" "ssh_private_key" {
 
 output "tls_private_key" { value = tls_private_key.ssh_private_key.private_key_pem }
 
+# Bootstrap deployment with wrapper script
+data "template_file" "wrapper_script" {
+  template = file("../userdata/installation-wrapper-script.sh")
+}
+
 # Create Ubuntu virtual machine and attach it to the virtual NIC.
 resource "azurerm_linux_virtual_machine" "opencti_vm" {
   name                  = "opencti_vm"
@@ -23,13 +28,15 @@ resource "azurerm_linux_virtual_machine" "opencti_vm" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "20.04-LTS"
+    sku       = "18.04-LTS" # time of writing: 20.04 not available (2021-02-09)
     version   = "latest"
   }
 
   computer_name                   = "opencti-vm" # underscores not allowed
   admin_username                  = "azureuser"
   disable_password_authentication = true
+  # Run wrapper script
+  custom_data = base64encode(data.template_file.wrapper_script.rendered)
 
   admin_ssh_key {
     username   = "azureuser"
@@ -40,10 +47,12 @@ resource "azurerm_linux_virtual_machine" "opencti_vm" {
     storage_account_uri = azurerm_storage_account.opencti_storage.primary_blob_endpoint
   }
 
-  # Deploy the OpenCTI wrapper script
-  provisioner "file" {
-    source                         = "../userdata/installation-wrapper-script.sh"
-    destination                    = "/opt"
+  # Control the connection for each provisioner in this resource.
+  connection {
+    type        = "ssh"
+    user        = "azureuser"
+    private_key = tls_private_key.ssh_private_key.private_key_pem
+    host        = azurerm_linux_virtual_machine.opencti_vm.name
   }
 
   # OpenCTI installer script
