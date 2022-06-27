8bb3ebe763c1f5b254172e0da9242589cff76653
Coleman Kane <ckane@colemankane.org>
2022-06-27 15:21:52 -0400

    Update with newer cred file specification
    This suppresses a warning

--- aws/main.tf
+++ aws/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region                  = var.region
-  shared_credentials_file = "~/.aws/credentials"
+  shared_credentials_files = ["$HOME/.aws/credentials"]
   profile                 = "default"
 }
 
